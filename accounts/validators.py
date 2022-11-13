1ec41206bb07dfd67b8d1bbb4445e4d355f091ce
Manjunatha31 <manjunathpoojary96@gmail.com>
2022-11-13 13:15:08 +0530

    Food Crud Operation

--- accounts/validators.py
+++ accounts/validators.py
@@ -4,6 +4,6 @@ import os
 def allow_only_images_validator(value):
     ext = os.path.splitext(value.name)[1] #cover-mage.jpg
     print(ext)
-    value_extension = ['.png','jpg','jpeg']
+    value_extension = ['.png','.jpg','.jpeg']
     if not ext.lower() in value_extension:
         raise ValidationError("Unsupported file extension. Allowed  extension"+str(value_extension))
