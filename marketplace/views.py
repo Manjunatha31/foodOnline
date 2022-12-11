4016347cd5672d8f0f310aba216fc97d8a6d1d18
Manjunatha31 <manjunathpoojary96@gmail.com>
2022-12-11 16:48:08 +0530

    business opening hour feature

--- marketplace/views.py
+++ marketplace/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render,redirect
 from .context_processors import get_cart_counter,get_cart_amounts
 from menu.models import Category, FoodItem
 
-from vendor.models import Vendor
+from vendor.models import Vendor,OpeningHour
 from django.db.models import Prefetch
 from .models import Cart
 from django.contrib.auth.decorators import login_required
@@ -11,6 +11,7 @@ from django.db.models import Q
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D
 from django.contrib.gis.db.models.functions import Distance
+from datetime import date,datetime
 
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
@@ -31,7 +32,17 @@ def vendor_detail(request, vendor_slug):
             queryset = FoodItem.objects.filter(is_available=True)
         )
     )
-
+    
+    opening_hours = OpeningHour.objects.filter(vendor = vendor).order_by('day','-from_hour')
+    # Check current day opening hours
+    today_date = date.today()
+    today = today_date.isoweekday()
+    current_opening_hours = OpeningHour.objects.filter(vendor = vendor,day = today)
+    
+  
+    
+    
+    
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
     else:
@@ -40,6 +51,9 @@ def vendor_detail(request, vendor_slug):
         'vendor': vendor,
         'categories': categories,
         'cart_items': cart_items,
+        'opening_hours':opening_hours,
+        'current_opening_hours':current_opening_hours,
+       
     }
     return render(request, 'marketplace/vendor_detail.html', context)
 
